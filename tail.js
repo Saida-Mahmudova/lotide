660126a2da99682bc75b6e6c3632f49715cd9352
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-08 03:19:05 +0300

    Implement tail func

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = array.slice(1);
+  if (array.length <= 1) {
+    return [];
+  }
+  return newArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
