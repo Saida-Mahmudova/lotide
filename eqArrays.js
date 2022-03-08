40682a37a7a5035a8de3e38a32ffd2a24efbada5
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-08 07:26:11 +0300

    Comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  let result;
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 8, 3]), false);
