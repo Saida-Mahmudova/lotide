5eb3e378858484f399fd5fc89af9a2c6d3a10e55
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-10 21:35:34 +0300

    Implement letterPosition func

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const eqArrays = function(first, second) {
+  for (let i in first) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let letter = 0; letter < sentence.length; letter++) {
+    if (sentence[letter] !== " ") {
+      if (results[sentence[letter]]) {
+        results[sentence[letter]].push(letter);
+      } else {
+        results[sentence[letter]] = [letter];
+      }
+    }
+  }
+  return results;
+};
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
