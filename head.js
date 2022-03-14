4da3031eaf28dde473cb0be4d4fc00fd7b145bb8
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-14 03:42:17 +0300

    Adding test code and refactoring of the function to DRY code

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 const head = function(array) {
   if (array.length > 0) {
     return array[0];
@@ -14,6 +7,4 @@ const head = function(array) {
   }
 };
 
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
