281feaf6d7b5b61403c84159b3819b7b00312cd3
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-14 17:06:16 +0300

    Using mocha/chai for assertion

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("tail", () => {
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
