a1de6800cbb51bfcccfcf7ffe530d6ac79eb8f51
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-14 03:46:26 +0300

    Adding testing code and refactoring the function

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = array.slice(1);
   if (array.length <= 1) {
@@ -14,6 +6,4 @@ const tail = function(array) {
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
